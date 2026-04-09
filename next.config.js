6528c2c48cd7a7dc2c49993ecc10906e86d13eca
Piotrek <piotr.styrylski3564@gmail.com>
2026-04-09 11:25:02 +0200

    feat: configure custom worker directory in next-pwa settings

--- next.config.js
+++ next.config.js
@@ -90,6 +90,7 @@ const withPWA = require('@ducanh2912/next-pwa').default({
     reloadOnOnline: true,
     swcMinify: true,
     disable: process.env.NODE_ENV === 'development',
+    customWorkerDir: 'src/worker',
     workboxOptions: {
         disableDevLogs: true,
     },
